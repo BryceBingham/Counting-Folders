b28bb1ddfc1bbb7674dd33fe84cde772ef66f843
BryceBingham <brycebingham04@gmail.com>
2023-03-09 10:03:47 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+ 
+public class Example
+{
+    public static bool isAlphaNumeric(char c)
+    {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+    }
+ 
+    public static void Main()
+    {
+        string alphOnly;
+        {
+            Console.WriteLine("Enter your sentance to count!");
+            string input = Console.ReadLine();
+            alphOnly = input;
+        }
+        alphOnly = String.Concat(Array.FindAll(alphOnly.ToCharArray(), isAlphaNumeric));
+        {
+            int result = alphOnly.Length;
+            Console.WriteLine("This is the amount of letters inside your sentance: " + result);
+        }
+    }
+}
